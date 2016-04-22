692475b2703f7310abc8cfad4237466ef5107c2a
Dominik Meißner <dominik.meissner@uni-ulm.de>
2016-04-22 19:36:37 +0200

    add boilerplate api

--- index.js
+++ index.js
@@ -1,17 +1,81 @@
 var fs = require('fs');
-var mysql = require("mysql");
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var config = JSON.parse(fs.readFileSync('config.json', 'UTF-8'));
 
-var con = mysql.createConnection(config.db);
+//var con = mysql.createConnection(config.db);
+//con.connect();
+//
+//connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//    if (err)
+//	throw err;
+//
+//    console.log('The solution is: ', rows[0].solution);
+//});
+//
+//con.end();
 
-con.connect();
+var app = express();
+app.use(bodyParser.json());
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err)
-	throw err;
+app.get('/v1/course', function(req, res) {
+    //TODO: retrieve courses from db
+    var courses = [
+	{
+	    id: 512,
+	    name: "Example Course",
+	    count: 1
+	}
+    ];
 
-    console.log('The solution is: ', rows[0].solution);
+    res.set('Content-Type', 'text/json')
+	.send(JSON.stringify(courses))
+	.status(200)
+	.end();
 });
 
-con.end();
+app.get('/v1/course/:id', function(req, res) {
+    var id = req.params.id;
+
+    //TODO: retrieve course from db
+
+    var course = {
+	id: id,
+	name: "Example Course",
+	documents: [
+	    {
+		id: 111,
+		type: "Klausur",
+		date: "2000-03-21",
+		lecturer: "Example Lecturer"
+	    }
+	]
+    };
+
+    if (course)
+	res
+	    .set('Content-Type', 'text/json')
+	    .send(JSON.stringify(course))
+	    .status(200)
+	    .end();
+    else
+	res
+	    .status(404)
+	    .end();
+});
+
+app.post('/v1/order', function(req, res) {
+    console.log(req.body);
+
+    //TODO: validate body
+    //TODO: send mail
+
+    res
+	.status(201)
+	.end();
+});
+
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
